ef8b4d2c0221b7c832e94f793c934f78d8db1dcb
Simon Weiß <simon.weiss@alexanderthamm.com>
2022-08-21 22:37:28 +0200

    Finish echo program, tests still open

---
+++ echor/src/main.rs
@@ -0,0 +1,26 @@
+use clap::{App, Arg};
+
+fn main() {
+    let _matches = App::new("echor")
+        .version("0.1.0")
+        .author("Simon Weiß <weiss.simon@outlook.com>")
+        .about("Echo written in Rust")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Text to echo")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Omit newline at the end")
+                .takes_value(false)
+        )
+        .get_matches();
+    let text = _matches.values_of_lossy("text").unwrap();
+    let omit_newline = _matches.is_present("omit_newline");
+
+    print!("{}{}", text.join(" "), if omit_newline {""} else {"\n"});
+}
---
+++ echor/tests/cli.rs
@@ -0,0 +1,21 @@
+use assert_cmd::Command;
+use predicates::prelude::*; 
+
+// #[test]
+// fn dies_no_args() {
+//     let mut cmd = Command::cargo_bin("echor").unwrap();
+//     cmd.assert() 
+//         .failure()
+//         .stderr(predicate::str::contains("USAGE"));
+// }
+#[test]
+fn dies_no_args() {
+    let mut cmd = Command::cargo_bin("echor").unwrap();
+    cmd.assert().failure().stderr(predicate::str::contains("USAGE"));
+}
+
+#[test]
+fn runs() {
+    let mut cmd = Command::cargo_bin("echor").unwrap();
+    cmd.arg("hello").assert().success();
+}
