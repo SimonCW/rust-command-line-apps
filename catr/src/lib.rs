8dc0d5dcea1da7107a94b557b03cc1fffbb8db87
Simon Weiß <simon.weiss@alexanderthamm.com>
2022-08-28 22:43:22 +0200

    Finish cat with -n and -b option

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 #[derive(Debug)]
 pub struct Config {
@@ -19,26 +21,62 @@ pub fn get_args() -> ProgResult<Config> {
             Arg::with_name("files")
                 .value_name("FILES")
                 .help("Files to cat")
-                .required(true)
-                .min_values(1),
+                .multiple(true)
+                .default_value("-"),
         )
         .arg(
             Arg::with_name("number_lines")
+                .long("number")
                 .short("-n")
                 .help("Print line numbers")
                 .takes_value(false),
         )
         .arg(
             Arg::with_name("number_nonblank_lines")
+                .long("number-nonblank")
                 .short("-b")
                 .help("Print line numbers for nonblank lines")
-                .takes_value(false),
+                .takes_value(false)
+                .conflicts_with("number_lines"),
         )
         .get_matches();
-    Ok(Config())
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        number_lines: matches.is_present("number_lines"),
+        number_nonblank_lines: matches.is_present("number_nonblank_lines"),
+    })
 }
 
-pub fn run() -> ProgResult<()> {
-    println!("Hello, world!");
+fn open(filename: &str) -> ProgResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+pub fn run(config: Config) -> ProgResult<()> {
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(file) => {
+                let mut last_num = 0;
+                for (line_num, line_result) in file.lines().enumerate() {
+                    let line = line_result?;
+                    if config.number_lines {
+                        println!("{:>6}\t{}", line_num + 1, line)
+                    } else if config.number_nonblank_lines {
+                        if !line.is_empty() {
+                            last_num += 1;
+                            println!("{:>6}\t{}", last_num, line);
+                        } else {
+                            println!();
+                        }
+                    } else {
+                        println!("{}", line);
+                    }
+                }
+            }
+        }
+    }
     Ok(()) // Return unit type in Ok variant to indicate success
 }
--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if let Err(e) = catr::run() {
+    if let Err(e) = catr::get_args().and_then(catr::run) {
         eprintln!("{}", e);
         std::process::exit(1);
     }
