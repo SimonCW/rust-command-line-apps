26def0bccc073fafab39dbb5a92b1f177d04a1f3
Simon Weiß <simon.weiss@alexanderthamm.com>
2022-09-10 19:24:16 +0200

    Finish program

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Read};
 
 type ProgResult<T> = Result<T, Box<dyn Error>>;
 
@@ -23,6 +25,7 @@ pub fn get_args() -> ProgResult<Config> {
         )
         .arg(
             Arg::with_name("lines")
+                .value_name("LINES")
                 .long("lines")
                 .short("n")
                 .takes_value(true)
@@ -31,21 +34,97 @@ pub fn get_args() -> ProgResult<Config> {
         )
         .arg(
             Arg::with_name("bytes")
+                .value_name("BYTES")
                 .long("bytes")
                 .short("c")
                 .takes_value(true)
                 .help("Number of bytes to print")
+                .required(false)
                 .conflicts_with("lines"),
         )
         .get_matches();
+
+    let lines = matches
+        .value_of("lines")
+        .map(parse_positive_int)
+        .transpose()
+        .map_err(|e| format!("illegal line count -- {}", e))?;
+
+    let bytes = matches
+        .value_of("bytes")
+        .map(parse_positive_int)
+        .transpose()
+        .map_err(|e| format!("illegal byte count -- {}", e))?;
+
     Ok(Config {
         files: matches.values_of_lossy("files").unwrap(),
-        lines: matches.value_of("lines").unwrap(),
-        bytes: matches.value_of("bytes"),
+        lines: lines.unwrap(),
+        bytes,
     })
 }
 
+fn parse_positive_int(val: &str) -> ProgResult<usize> {
+    match val.parse() {
+        Ok(n) if n > 0 => Ok(n),
+        _ => Err(From::from(val)),
+    }
+}
+#[test]
+fn test_parse_positive_int() {
+    // 3 is an OK integer
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    // Any string is an error
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    // A zero is an error
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
+}
+
+fn open(filename: &str) -> ProgResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
 pub fn run(config: Config) -> ProgResult<()> {
-    println!("{:#?}", config);
+    let n_files = config.files.len();
+    for (file_num, filename) in config.files.iter().enumerate() {
+        match open(filename) {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(mut file) => {
+                if n_files > 1 {
+                    println!(
+                        "{}==> {} <==",
+                        if file_num > 0 { "\n" } else { "" },
+                        filename
+                    )
+                }
+                if let Some(num_bytes) = config.bytes {
+                    let mut handle = file.take(num_bytes as u64);
+                    let mut buffer = vec![0; num_bytes];
+                    let bytes_read = handle.read(&mut buffer)?;
+                    print!("{}", String::from_utf8_lossy(&buffer[..bytes_read]));
+                } else {
+                    let mut line = String::new();
+                    for _ in 0..config.lines {
+                        let bytes = file.read_line(&mut line)?;
+                        if bytes == 0 {
+                            break;
+                        }
+                        print!("{}", line);
+                        line.clear()
+                    }
+                }
+            }
+        }
+    }
     Ok(())
 }
