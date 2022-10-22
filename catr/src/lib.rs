7b303cd9b48ea558d8fa74c5b881b440c0392871
Simon Weiß <simon.weiss@alexanderthamm.com>
2022-10-22 20:51:52 +0200

    Migrate catr to clap 4

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,59 +1,33 @@
-use clap::{App, Arg};
+use clap::Parser;
 use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
-#[derive(Debug)]
+type ProgResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
 pub struct Config {
+    /// Files to cat
+    #[arg(name = "FILES", default_value = "-")]
     files: Vec<String>,
+    /// Print line numbers
+    #[arg(short, long = "number")]
     number_lines: bool,
+
+    /// Print line numbers for nonblank lines
+    #[arg(short = 'b', long = "number-nonblank", conflicts_with = "number_lines")]
     number_nonblank_lines: bool,
+
+    /// Show $ at the end of each line
+    #[arg(short = 'E', long = "show-ends")]
     show_ends: bool,
 }
 
-type ProgResult<T> = Result<T, Box<dyn Error>>;
-
 pub fn get_args() -> ProgResult<Config> {
-    let matches = App::new("catr")
-        .version("0.1.0")
-        .author("Simon Weiß")
-        .about("cat in Rust")
-        .arg(
-            Arg::with_name("files")
-                .value_name("FILES")
-                .help("Files to cat")
-                .multiple(true)
-                .default_value("-"),
-        )
-        .arg(
-            Arg::with_name("number_lines")
-                .long("number")
-                .short("-n")
-                .help("Print line numbers")
-                .takes_value(false),
-        )
-        .arg(
-            Arg::with_name("number_nonblank_lines")
-                .long("number-nonblank")
-                .short("-b")
-                .help("Print line numbers for nonblank lines")
-                .takes_value(false)
-                .conflicts_with("number_lines"),
-        )
-        .arg(
-            Arg::with_name("show_ends")
-                .long("show-ends")
-                .short("-E")
-                .help("Show $ at the end of each line")
-                .takes_value(false),
-        )
-        .get_matches();
-    Ok(Config {
-        files: matches.values_of_lossy("files").unwrap(),
-        number_lines: matches.is_present("number_lines"),
-        number_nonblank_lines: matches.is_present("number_nonblank_lines"),
-        show_ends: matches.is_present("show_ends"),
-    })
+    let config = Config::parse();
+    // println!("{:?}", config);
+    Ok(config)
 }
 
 fn open(filename: &str) -> ProgResult<Box<dyn BufRead>> {
